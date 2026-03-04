3cf6089d16eecd6734ba1cfca31c64e129b51434
Glimsura <antihete12@gmail.com>
2026-03-04 23:33:59 +0200

    fix: use RouterProvider only (remove duplicate BrowserRouter/App)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { ProfileProvider } from "./context/ProfileContext.jsx";
 import "./styles/globals.css";
@@ -11,11 +10,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <ProfileProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </ProfileProvider>
     </ThemeProvider>
-    <RouterProvider router={router} />
   </React.StrictMode>
 );
