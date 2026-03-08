b0091f6ed75bc18ddb1817f332f0692ca95f028d
Glimsura <antihete12@gmail.com>
2026-03-08 21:11:48 +0200

    fix: vercel.json runtime config (use default serverless)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { ProfileProvider } from "./context/ProfileContext.jsx";
 import "./styles/globals.css";
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <ProfileProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ProfileProvider>
     </ThemeProvider>
   </React.StrictMode>
